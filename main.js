adf941b18ab89ed86766c7064b214927ff1303bb
shinat2002 <157089830+shinat2002@users.noreply.github.com>
2024-02-12 15:44:40 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
-document.querySelector("chanterelle-button").addEventListener("click", function()
-alert("you clicked a button"))
+const header = document.querySelector("#header")
+const image = document.querySelector("#image")
+const credit = document.querySelector("#credit")
+
+document.querySelector("#chanterelle-button").addEventListener("click", function() [
+   header.innerHTML = "Chanterelle"
+    image.src = "chanterelle.jpg"
+    credit.innerHTML = "copyright Neil Leclerc"
+])
+
+document.querySelector("#oyster-button").addEventListener("click", function() [
+    header.innerHTML = "Oyster mushroom"
+    image.src = "oyster.jpg"
+    credit.innerHTML = "copyright Robbie Sproule"
+])
+
+document.querySelector("#fly-agaric-button").addEventListener("click", function() [
+    header.innerHTML = "Fly agaric"
+    image.src = "fly-agaric.jpg"
+    credit.innerHTML = "copyright Victoria Vasilieva" 
+])
